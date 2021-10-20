c7fcf47d437a4d11caf888c96e86ff09f00cfecf
thomHayner <thom.hayner@gmail.com>
2021-10-20 02:10:48 -0600

    add ui/ux for droppable

--- src/Components/Ships/Submarine.js
+++ src/Components/Ships/Submarine.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Square from '../Board/Square';
+
+const ship = [4,4,4];
 
 const Submarine = (props) => {
+    if (props.afloat === null) {
+        return (<div> { ship.map(x=><Square/>) } </div>)
+    }
     if (props.afloat === true) {
         return (<div className='submarine' >Submarine</div>)
     } else {
