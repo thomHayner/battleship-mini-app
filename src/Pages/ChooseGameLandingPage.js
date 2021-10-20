d93a472f5cb89b1378bcbeaf7aeb1de544a00d65
thomHayner <thom.hayner@gmail.com>
2021-10-20 00:38:21 -0600

    start making 2 dropables, to place ships on board

--- src/Pages/ChooseGameLandingPage.js
+++ src/Pages/ChooseGameLandingPage.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Board from "../Components/Board/Board";
+import Carrier from "../Components/Ships/Carrier";
+import Destroyer from "../Components/Ships/Destroyer";
+import PatrolBoat from "../Components/Ships/PatrolBoat";
+import Submarine from "../Components/Ships/Submarine";
+import BattleShip from "../Components/Ships/Battleship";
 import ShipPlacer from "../Logic/ShipPlacer";
 
 function ChooseGame() {
@@ -23,16 +28,70 @@ function ChooseGame() {
   
   return (
     <div>
-      <DragDropContext>
-        <div>
-          <Board board={playerBoard} player={playerName} handleFire={ ShipPlacer } onHover={ ShipPlacer }  />
-        </div>
-        <div>
-
-        </div>
+      <DragDropContext onDragEnd={result=>console.log(result)}>
+        <Droppable droppableId={'droppable-1'}>
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+              {...provided.droppableProps}
+            >
+              <h2>I am a droppable!</h2>
+              <div>
+                <Board board={playerBoard} player={playerName} handleFire={ ShipPlacer } onHover={ ShipPlacer }  />
+              </div>
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <Droppable droppableId={'droppable-2'}>
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+              {...provided.droppableProps}
+            >
+              <h2>I am a droppable!</h2>
+              
+              <Draggable draggableId="draggable-1" index={0}>
+              (provided, snapshot) => (
+              <div
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              >
+                    <h4>My draggable</h4>
+                    <Submarine afloat={true} />
+                  </div>
+                )}
+            </Draggable>;
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        {/* <Droppable droppableId={'droppable-2'}>
+          <div id={"looseShipHolder"}>
+            <Draggable draggableId='draggable-1' index={0}>
+              <Submarine afloat={true} />
+            </Draggable>
+          </div>
+        </Droppable> */}
       </DragDropContext>
     </div>
   )
 }
 
 export default ChooseGame
+
+{/* <Draggable>
+            <Carrier />
+          </Draggable>
+          <Draggable>
+            <Destroyer />
+          </Draggable>
+          <Draggable>
+            <PatrolBoat />
+          </Draggable>
+          <Draggable>
+            <BattleShip />
+          </Draggable> */}
