9ed199efb585e869c6e4bd6f6870ff13b7daa3b8
thomHayner <thom.hayner@gmail.com>
2021-10-20 01:55:30 -0600

    add display for board placement

--- src/Components/Ships/Battleship.js
+++ src/Components/Ships/Battleship.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Square from '../Board/Square';
+
+const ship = [2,2,2,2]
 
 const Battleship = (props) => {
+    if (props.afloat === null) {
+        return (
+        <div>
+            { ship.map(x=><Square/>) }
+        </div>)
+    }
     if (props.afloat === true) {
         return (<div className='battleship' >Battleship</div>)
     } else {
