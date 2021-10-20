8992c0fc8b915b7db4e10e3fb8f9d02e5b1b99bf
thomHayner <thom.hayner@gmail.com>
2021-10-19 23:22:18 -0600

    add some more component basics

--- src/Pages/ChooseGameLandingPage.js
+++ src/Pages/ChooseGameLandingPage.js
@@ -1,28 +1,36 @@
 import React, { useState } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
 import Board from "../Components/Board/Board";
-import FireControlHandler from "../Logic/FireControlHandler";
+import ShipPlacer from "../Logic/ShipPlacer";
 
 function ChooseGame() {
   const [playerBoard, setPlayerBoard] = useState(
     [
       ["","1","2","3","4","5","6","7","8","9","10",],
-      ["A",2,0,0,0,0,0,0,0,0,4],
-      ["B",2,0,0,0,0,0,0,0,0,4],
-      ["C",2,0,0,0,0,0,0,0,0,4],
-      ["D",2,0,0,0,0,0,0,0,0,0],
+      ["A",0,0,0,0,0,0,0,0,0,0],
+      ["B",0,0,0,0,0,0,0,0,0,0],
+      ["C",0,0,0,0,0,0,0,0,0,0],
+      ["D",0,0,0,0,0,0,0,0,0,0],
       ["E",0,0,0,0,0,0,0,0,0,0],
       ["F",0,0,0,0,0,0,0,0,0,0],
-      ["G",3,3,3,0,0,0,0,0,0,0],
+      ["G",0,0,0,0,0,0,0,0,0,0],
       ["H",0,0,0,0,0,0,0,0,0,0],
       ["I",0,0,0,0,0,0,0,0,0,0],
-      ["J",1,1,1,1,1,0,0,0,5,5]
+      ["J",0,0,0,0,0,0,0,0,0,0]
     ]
   );
   const [playerName, setPlayerName] = useState("Player 1");
   
   return (
     <div>
-      <Board board={playerBoard} player={playerName} handleFire={ FireControlHandler } />
+      <DragDropContext>
+        <div>
+          <Board board={playerBoard} player={playerName} handleFire={ ShipPlacer } onHover={ ShipPlacer }  />
+        </div>
+        <div>
+
+        </div>
+      </DragDropContext>
     </div>
   )
 }
