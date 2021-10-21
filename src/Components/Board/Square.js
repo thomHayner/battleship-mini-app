3d6d38b36798915f76f4bcd512ac6dff1ecf40bf
thomHayner <thom.hayner@gmail.com>
2021-10-21 13:34:52 -0600

    add droppable area option for squares

--- src/Components/Board/Square.js
+++ src/Components/Board/Square.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const Square = (props) => {
         if (typeof props.value === 'string') {
@@ -7,7 +8,24 @@ const Square = (props) => {
             return(<div className="confirmedMiss" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } ></div>)
         } else if (props.value === -1) {
             return(<div className="confirmedHit" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } ></div>)
-        } else {
+        } else if (props.value === -3) {
+            return(
+                <DragDropContext onDragEnd={result=>console.log(result)}>
+                    <Droppable droppableId={`droppable-${props.row}${props.col}`} className="square">
+                        {(provided, snapshot) => (
+                            <div
+                            ref={provided.innerRef}
+                            style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+                            {...provided.droppableProps}
+                            >
+                            {provided.placeholder}
+                            <div className="confirmedHit" onClick={ (e) => props.handleFire(()=>{}) } />
+                        </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+            )
+        }else {
             return(<div className="square" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } ></div>)
         }
 };
