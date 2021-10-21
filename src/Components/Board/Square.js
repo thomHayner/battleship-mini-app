eb839a0f0df5c3e6f708df2e6641111d832f6e1a
thomHayner <thom.hayner@gmail.com>
2021-10-21 15:53:06 -0600

    code cleanup and comments

--- src/Components/Board/Square.js
+++ src/Components/Board/Square.js
@@ -2,29 +2,32 @@ import React from 'react';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const Square = (props) => {
-        if (typeof props.value === 'string') {
+        if (props.afloat === null) {
+            return 
+        } else if (typeof props.value === 'string') {
             return(<div className="index-square" >{props.value}</div>)
         } else if (props.value === -2) {
-            return(<div className="confirmedMiss" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } ></div>)
+            return <div className="confirmedMiss" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } />
         } else if (props.value === -1) {
-            return(<div className="confirmedHit" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } ></div>)
-        } else if (props.value === -3) {
+            return <div className="confirmedHit" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } />
+        } else if (props.value === -3) { // -3 is used for the droppables screen
             return(
-                    <Droppable droppableId={`droppable-${props.row}${props.col}`} className="square">
+                    <Droppable droppableId={`droppable-${props.row}-${props.col}`} className="square">
                         {(provided, snapshot) => (
                             <div
                             ref={provided.innerRef}
-                            style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+                            style={{ backgroundColor: snapshot.isDraggingOver ? 'green' : 'grey' }}
                             {...provided.droppableProps}
+                            {...provided.dragHandleProps} // is this necessary?
                             >
-                            {provided.placeholder}
-                            <div className="confirmedHit" onClick={ (e) => props.handleFire(()=>{}) } />
-                        </div>
+                                {provided.placeholder}
+                                <div className="confirmedHit" onClick={ (e) => props.handleFire(()=>{}) } />
+                            </div>
                         )}
                     </Droppable>
             )
-        }else {
-            return(<div className="square" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } ></div>)
+        }  else {
+            return <div className="square" onClick={ (e) => props.handleFire( props.row, props.col, props.player ) } />
         }
 };
 
