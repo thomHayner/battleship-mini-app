95ff1dea2898b00f1865cbb312b3d53192a95b0e
thomHayner <thom.hayner@gmail.com>
2021-10-21 18:55:52 -0600

    move draggable to ship component

--- src/Components/Ships/Submarine.js
+++ src/Components/Ships/Submarine.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Square from '../Board/Square';
+import { Draggable } from 'react-beautiful-dnd';
 
-const ship = [4,4,4];
+const ship = [[4],[4],[4]];
 
 const Submarine = (props) => {
     if (props.afloat === null) {
-        return (<div style={{ display: 'flex' }}> { ship.map(x=><Square/>) } </div>)
+        return (
+            <Draggable draggableId='submarine' index={0} type='SHIP'>
+                {(provided, snapshot) => (
+                    <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    >
+                        <div style={{ display: 'flex' }}>
+                            { ship.map(x=>[<Square/>]) }
+                        </div>
+                    </div>
+                )}
+            </Draggable>
+        )
     }
     if (props.afloat === true) {
         return (<div className='submarine' >Submarine</div>)
