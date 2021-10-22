cd129ea96ed9148151c77df6fb772acc4b1fb38b
thomHayner <thom.hayner@gmail.com>
2021-10-21 19:09:11 -0600

    move <Draggable> out to component

--- src/Components/Ships/Destroyer.js
+++ src/Components/Ships/Destroyer.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Square from '../Board/Square';
+import { Draggable } from 'react-beautiful-dnd';
 
 const ship = [5,5];
 
 const Destroyer = (props) => {
     if (props.afloat === null) {
-        return (<div style={{ display: 'flex' }}> { ship.map(x=><Square/>) } </div>)
+        return (
+            <Draggable draggableId='Destroyer' index={0} type='SHIP'>
+                {(provided, snapshot) => (
+                    <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    >
+                        <div style={{ display: 'flex' }}>
+                            { ship.map(x=>[<Square/>]) }
+                        </div>
+                    </div>
+                )}
+            </Draggable>
+        )
     }
     if (props.afloat === true) {
         return (<div className='destroyer' >Destroyer</div>)
