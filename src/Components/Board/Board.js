5909af01c556918ef05b3cd5522d6007c8a96de5
thomHayner <thom.hayner@gmail.com>
2021-11-17 21:00:51 -0700

    code: cleanup

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -4,14 +4,16 @@ import Square from './Square.js';
 const Board = (props) => 
     <div className="board" >
         {props.board.map( (i, row) => 
-            props.board.map( (j, col) => <Square 
-                                            key={[row, col]} 
-                                            value={props.board[row][col]} 
-                                            row={row} 
-                                            col={col} 
-                                            player={props.player} 
-                                            handleFire={props.handleFire} 
-                                        />)
+            props.board.map( (j, col) => (
+                <Square 
+                    key={[row, col]} 
+                    value={props.board[row][col]} 
+                    row={row} 
+                    col={col} 
+                    player={props.player} 
+                    handleFire={props.handleFire} 
+                />
+            ))
         )}
     </div>;
 
