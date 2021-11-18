3c5a21b5a103be1f8fa89dc867e42077a37a23f2
thomHayner <thom.hayner@gmail.com>
2021-11-18 02:18:53 -0700

    code: build component

--- src/Components/ScoreCard.js
+++ src/Components/ScoreCard.js
@@ -1,7 +1,22 @@
+
+import ShipList from "./ShipList";
+
 export default function ScoreCard({ name }) {
+  let var1;
+  let var2;
+
+  if (name === "redPlayer") { 
+    var1 = "redPlayer"
+    var2 = "bluePlayer"
+  } else {
+    var1 = "bluePlayer"
+    var2 = "redPlayer"
+  }
   return (
     <div>
-
+      <ShipList name={var1} />
+      <div/>
+      <ShipList name={var2} />
     </div>
   )
 }
