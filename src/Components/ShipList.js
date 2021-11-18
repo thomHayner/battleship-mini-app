d527c64ef9037e40249752fa1380dbf02feffb87
thomHayner <thom.hayner@gmail.com>
2021-11-18 02:19:14 -0700

    code: build component

--- src/Components/ShipList.js
+++ src/Components/ShipList.js
@@ -1,7 +1,18 @@
+import Carrier from "./Ships/Carrier";
+import Battleship from "./Ships/Battleship";
+import Destroyer from "./Ships/Destroyer";
+import Submarine from "./Ships/Submarine";
+import Cruiser from "./Ships/Cruiser";
+
 export default function ShipList({ name }) {
   return (
-    <div>
-
+    <div className="enemy-ship-list" >
+      <div><strong>Enemy Ships:</strong></div>
+      <Carrier afloat={this.state.bluePlayer.carrierAfloat} />
+      <Battleship afloat={this.state.bluePlayer.battleshipAfloat} />
+      <Destroyer afloat={this.state.bluePlayer.cruiserAfloat} />
+      <Submarine afloat={this.state.bluePlayer.submarineAfloat} />
+      <Cruiser afloat={this.state.bluePlayer.destroyerAfloat} />
     </div>
   )
 }
