4f689b4ab6486cc998695392357c16892e729056
thomHayner <thom.hayner@gmail.com>
2021-11-18 04:47:46 -0700

    content: fix errors in messages displayed to users

--- src/Components/DisplayMessages/WhoseTurnIsItAnyway.js
+++ src/Components/DisplayMessages/WhoseTurnIsItAnyway.js
@@ -3,11 +3,11 @@ import React from 'react';
 const WhoseTurnIsItAnyway = (props) => {
     if (props.turn === true) {
         return (
-            <div className="whose-turn" ><h3>Player 2 is preparing to fire.</h3></div>
+            <div className="whose-turn" ><h3>Blue Player is preparing to fire.</h3></div>
         )
     } else {
         return (
-            <div className="whose-turn" ><h3>Player 1 is preparing to fire.</h3></div>
+            <div className="whose-turn" ><h3>Red Player is preparing to fire.</h3></div>
         )
     }
 }
