2079028ffb17cdeaff63c636e809f6c06cf3431d
thomHayner <thom.hayner@gmail.com>
2021-11-18 04:45:45 -0700

    code: cleanup, rename props

--- src/Components/PlayerDashboard.js
+++ src/Components/PlayerDashboard.js
@@ -6,12 +6,12 @@ import '../App.css';
 
 // Takes in a player object (redPlayer or BluePlayer) and outputs a game board stacked on top of a scorecard
 
-export default function PlayerDashboard({ player, opponent, handleFire }) {
+export default function PlayerDashboard({ player, opponentShips, handleFire }) {
   return (
     <div>
       <PlayerName name={player.name} />
       <Board board={player.board} name={player.name} handleFire={handleFire} />
-      <ScoreCard player={player} opponent={opponent} />
+      <ScoreCard player={player} opponentShips={opponentShips} />
     </div>
   )
 }
