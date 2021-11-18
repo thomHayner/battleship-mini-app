59b86c26c36f4c3ac3988854338bb61fae9a81b0
thomHayner <thom.hayner@gmail.com>
2021-11-17 21:01:52 -0700

    chore: comment out homeless code file

--- src/fireControlHandler.js
+++ src/fireControlHandler.js
@@ -1,127 +1,127 @@
-fireControlHandler = (row, col) => {
-    if (this.state.win.length === 35) {
-      let newShot = this.state.shot;
-      let nowSinking = this.state.sinking;
-      let newWin = this.state.win;
-      let roundBoard;
-      let carrier;
-      let battleship;
-      let destroyer;
-      let submarine;
-      let patrolBoat;
+// fireControlHandler = (row, col) => {
+//     if (this.state.win.length === 35) {
+//       let newShot = this.state.shot;
+//       let nowSinking = this.state.sinking;
+//       let newWin = this.state.win;
+//       let roundBoard;
+//       let carrier;
+//       let battleship;
+//       let destroyer;
+//       let submarine;
+//       let patrolBoat;
 
-      if (this.state.isTheEnemyFiring === true ) {
-        roundBoard = this.state.enemyBoard.slice();
-        carrier = this.state.enemyCarrierAfloat;
-        battleship = this.state.enemyBattleshipAfloat;
-        destroyer = this.state.enemyDestroyerAfloat;
-        submarine = this.state.enemySubmarineAfloat;
-        patrolBoat = this.state.enemyPatrolBoatAfloat;
-      } else {
-        roundBoard = this.state.playerBoard.slice()
-        carrier = this.state.playerCarrierAfloat;
-        battleship = this.state.playerBattleshipAfloat;
-        destroyer = this.state.playerDestroyerAfloat;
-        submarine = this.state.playerSubmarineAfloat;
-        patrolBoat = this.state.playerPatrolBoatAfloat;
-      }
+//       if (this.state.isTheEnemyFiring === true ) {
+//         roundBoard = this.state.enemyBoard.slice();
+//         carrier = this.state.enemyCarrierAfloat;
+//         battleship = this.state.enemyBattleshipAfloat;
+//         destroyer = this.state.enemyDestroyerAfloat;
+//         submarine = this.state.enemySubmarineAfloat;
+//         patrolBoat = this.state.enemyPatrolBoatAfloat;
+//       } else {
+//         roundBoard = this.state.playerBoard.slice()
+//         carrier = this.state.playerCarrierAfloat;
+//         battleship = this.state.playerBattleshipAfloat;
+//         destroyer = this.state.playerDestroyerAfloat;
+//         submarine = this.state.playerSubmarineAfloat;
+//         patrolBoat = this.state.playerPatrolBoatAfloat;
+//       }
 
-      if (roundBoard[row][col] > 0) {
-        roundBoard[row][col] = -1;
-        newShot = 'Firing on ' + roundBoard[row][0] + col + '...Direct Hit!';
-      }
-      else if (roundBoard[row][col] === 0) {
-        roundBoard[row][col] = -2;
-        newShot = 'Firing on ' + roundBoard[row][0] + col + '...Miss!';
-      }
-      else if (roundBoard[row][col] < 0) {
-        newShot = 'You have already fired on this location. Try Again.';
-      }
+//       if (roundBoard[row][col] > 0) {
+//         roundBoard[row][col] = -1;
+//         newShot = 'Firing on ' + roundBoard[row][0] + col + '...Direct Hit!';
+//       }
+//       else if (roundBoard[row][col] === 0) {
+//         roundBoard[row][col] = -2;
+//         newShot = 'Firing on ' + roundBoard[row][0] + col + '...Miss!';
+//       }
+//       else if (roundBoard[row][col] < 0) {
+//         newShot = 'You have already fired on this location. Try Again.';
+//       }
 
-      let checkWinner = function(roundBoard) {
-        let tracker = [0,0,0,0,0];
+//       let checkWinner = function(roundBoard) {
+//         let tracker = [0,0,0,0,0];
 
-        for (let i = 1; i < 11; i++) {
-          for (let j = 1; j < 11; j++) {
-            switch ( roundBoard[i][j] ) {
-              case 1:
-                tracker[0]++
-                break;
-              case 2:
-                tracker[1]++
-                break;
-              case 3:
-                tracker[2]++
-                break;
-              case 4:
-                tracker[3]++
-                break;
-              case 5:
-                tracker[4]++
-                break;
-              default:
-                ;
-            }
-          }
-        }
-        return tracker;
-      }
+//         for (let i = 1; i < 11; i++) {
+//           for (let j = 1; j < 11; j++) {
+//             switch ( roundBoard[i][j] ) {
+//               case 1:
+//                 tracker[0]++
+//                 break;
+//               case 2:
+//                 tracker[1]++
+//                 break;
+//               case 3:
+//                 tracker[2]++
+//                 break;
+//               case 4:
+//                 tracker[3]++
+//                 break;
+//               case 5:
+//                 tracker[4]++
+//                 break;
+//               default:
+//                 ;
+//             }
+//           }
+//         }
+//         return tracker;
+//       }
 
-      let tracker = checkWinner(roundBoard);
+//       let tracker = checkWinner(roundBoard);
 
-      if (tracker[0] === 0 && carrier === true) {
-        nowSinking = 'The enemy\'s Aircraft Carrier is sinking!';
-        carrier= false;
-      };
-      if (tracker[1] === 0 && battleship === true) {
-        nowSinking = 'The enemy\'s Battleship is sinking!';
-        battleship = false;
-      };
-      if (tracker[2] === 0 && destroyer === true) {
-        nowSinking = 'The enemy\'s Destroyer is sinking!';
-        destroyer = false;
-      };
-      if (tracker[3] === 0 && submarine === true) {
-        nowSinking = 'The enemy\'s Submarine is sinking!';
-        submarine = false;
-      };
-      if (tracker[4] === 0 && patrolBoat === true) {
-        nowSinking = 'The enemy\'s Patrol Boat is sinking!';
-        patrolBoat = false;
-      };
-      if (tracker.reduce(((a, b) => a+b), 0) === 0) {
-        newShot = 'VICTORY!';
-        newWin = 'You have sunk all of the enemy\'s ships!';
-      };
+//       if (tracker[0] === 0 && carrier === true) {
+//         nowSinking = 'The enemy\'s Aircraft Carrier is sinking!';
+//         carrier= false;
+//       };
+//       if (tracker[1] === 0 && battleship === true) {
+//         nowSinking = 'The enemy\'s Battleship is sinking!';
+//         battleship = false;
+//       };
+//       if (tracker[2] === 0 && destroyer === true) {
+//         nowSinking = 'The enemy\'s Destroyer is sinking!';
+//         destroyer = false;
+//       };
+//       if (tracker[3] === 0 && submarine === true) {
+//         nowSinking = 'The enemy\'s Submarine is sinking!';
+//         submarine = false;
+//       };
+//       if (tracker[4] === 0 && patrolBoat === true) {
+//         nowSinking = 'The enemy\'s Patrol Boat is sinking!';
+//         patrolBoat = false;
+//       };
+//       if (tracker.reduce(((a, b) => a+b), 0) === 0) {
+//         newShot = 'VICTORY!';
+//         newWin = 'You have sunk all of the enemy\'s ships!';
+//       };
       
-      if (this.state.isTheEnemyFiring === true ) {
-        this.setState({ 
-            enemyBoard: roundBoard, 
-            enemyCarrierAfloat: carrier,
-            enemyBattleshipAfloat: battleship,
-            enemyDestroyerAfloat: destroyer,
-            enemySubmarineAfloat: submarine,
-            enemyPatrolBoatAfloat: patrolBoat,
-        });
-      } else {
-        this.setState({ 
-            playerBoard: roundBoard, 
-            playerCarrierAfloat: carrier,
-            playerBattleshipAfloat: battleship,
-            playerDestroyerAfloat: destroyer,
-            playerSubmarineAfloat: submarine,
-            playerPatrolBoatAfloat: patrolBoat,
-        });
-      }
+//       if (this.state.isTheEnemyFiring === true ) {
+//         this.setState({ 
+//             enemyBoard: roundBoard, 
+//             enemyCarrierAfloat: carrier,
+//             enemyBattleshipAfloat: battleship,
+//             enemyDestroyerAfloat: destroyer,
+//             enemySubmarineAfloat: submarine,
+//             enemyPatrolBoatAfloat: patrolBoat,
+//         });
+//       } else {
+//         this.setState({ 
+//             playerBoard: roundBoard, 
+//             playerCarrierAfloat: carrier,
+//             playerBattleshipAfloat: battleship,
+//             playerDestroyerAfloat: destroyer,
+//             playerSubmarineAfloat: submarine,
+//             playerPatrolBoatAfloat: patrolBoat,
+//         });
+//       }
 
-      this.setState({ 
-          isTheEnemyFiring: !this.state.isTheEnemyFiring, 
-          shot: newShot, 
-          sinking: nowSinking, 
-          win: newWin,
-      });
-    }
-  }
+//       this.setState({ 
+//           isTheEnemyFiring: !this.state.isTheEnemyFiring, 
+//           shot: newShot, 
+//           sinking: nowSinking, 
+//           win: newWin,
+//       });
+//     }
+//   }
 
 
 
