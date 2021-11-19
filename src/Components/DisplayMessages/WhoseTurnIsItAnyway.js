f39c5ba0026597a6716fa9820705adc65535176f
thomHayner <thom.hayner@gmail.com>
2021-11-19 02:24:27 -0700

    content: change player names

--- src/Components/DisplayMessages/WhoseTurnIsItAnyway.js
+++ src/Components/DisplayMessages/WhoseTurnIsItAnyway.js
@@ -3,11 +3,11 @@ import React from 'react';
 const WhoseTurnIsItAnyway = (props) => {
     if (props.turn === true) {
         return (
-            <div className="whose-turn" ><h3>Blue Player is preparing to fire.</h3></div>
+            <div className="whose-turn" ><h3>Computer is preparing to fire.</h3></div>
         )
     } else {
         return (
-            <div className="whose-turn" ><h3>Red Player is preparing to fire.</h3></div>
+            <div className="whose-turn" ><h3>Player is preparing to fire.</h3></div>
         )
     }
 }
