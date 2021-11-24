f87c7955953e2aad7849a9dc6a6407f3c9a8a737
thomHayner <thom.hayner@gmail.com>
2021-11-23 21:49:10 -0700

    code: refactor to make more dynamic

--- src/Components/Elements/Square.js
+++ src/Components/Elements/Square.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Square = ({ value, row, col, playerId, ...props }) => {
+const Square = ({ value, ...props }) => {
   if (typeof value === 'string') {
     return <div className="index-square" >{value}</div>
   } else if (value === -1) {
     return(
       <div 
         className="placement-square-fill" 
-        onDragStart={e=>props.onDragStart(e)} 
+        // onDragStart={e=>props.onDragStart(e)} 
       />
     )
   } else if (value === -2) {
@@ -22,13 +22,13 @@ const Square = ({ value, row, col, playerId, ...props }) => {
       />
     )
   } else if (value === -3) {
-    return <div className="confirmed-hit" onClick={ (e) => props.handleFire(row, col, playerId) } />
+    return <div className="confirmed-hit" onClick={ (e) => props.handleFire(props.row, props.col, props.playerId) } />
   } else if (value === -4) {
-    return <div className="confirmed-miss" onClick={ (e) => props.handleFire(row, col, playerId) } />
+    return <div className="confirmed-miss" onClick={ (e) => props.handleFire(props.row, props.col, props.playerId) } />
   } else if (value === -5) {
     return <div className="placement-square-draggable" />
   } else {
-    return <div className="square" onClick={ (e) => props.handleFire(row, col, playerId) } />
+    return <div className="square" onClick={ (e) => props.handleFire(props.row, props.col, props.playerId) } />
   }
 };
 
