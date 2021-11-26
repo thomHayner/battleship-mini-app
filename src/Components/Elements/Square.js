f90019e37d179590a289eddf5fd7c45f8d058699
thomHayner <thom.hayner@gmail.com>
2021-11-25 19:19:39 -0700

    code: cleanup

--- src/Components/Elements/Square.js
+++ src/Components/Elements/Square.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import './square.css';
 
-const Square = ({ value, ...props }) => {
-  const StandardGameBoardSquareDiv = (tempClassName) => <div className={tempClassName} onClick={ (e) => props.handleFire(props.row, props.col, props.playerId) } />;
+const Square = ({ value, row, col, ...props }) => {
+  const StandardGameBoardSquareDiv = (tempClassName) => <div className={tempClassName} onClick={ (e) => props.handleFire(row, col, props.playerId) } />;
   
   if (typeof value === 'string') {
     return <div className="index-square" >{value}</div>
   } else if (value === -1) {
     return <div className="placement-square-fill" />
   } else if (value === -2) {
-      let squareId = props.row * props.col;
     return(
-      <div 
+        <div 
         className="placement-square-empty" 
         // onClick={ (e) => props.handleChangeDirection } 
-        onDragEnter={e=>props.onDragEnter(e, squareId)} 
-        onDragLeave={e=>props.onDragLeave(e, squareId)} 
+        onDragEnter={e=>props.onDragEnter(e, row, col)} 
+        onDragLeave={e=>props.onDragLeave(e, row, col)} 
         onDragOver={e=>props.onDragOver(e)} 
-        onDrop={e=>props.onDrop(e, props.row, props.col)} 
+        onDrop={e=>props.onDrop(e, row, col)} 
       />
     )
   } else if (value === -3) {
-    return StandardGameBoardSquareDiv("confirmed-hit")
+    return StandardGameBoardSquareDiv("confirmed-hit");
   } else if (value === -4) {
-    return StandardGameBoardSquareDiv("confirmed-miss")
+    return StandardGameBoardSquareDiv("confirmed-miss");
   } else if (value > 0 && props.gameStart === false) {
-    return StandardGameBoardSquareDiv("ship")
+    return(
+        <div 
+            className="ship" 
+            // onClick={ (e) => props.handleChangeDirection } 
+        />
+    )
   } else {
-    return StandardGameBoardSquareDiv("square")
+    return StandardGameBoardSquareDiv("square");
   }
 };
 
